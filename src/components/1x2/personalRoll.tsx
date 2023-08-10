604fe17593e35f7f7eed41959f151aa301d34de9
Wladimir Sanvicente <wladimirss14@gmail.com>
2023-08-09 20:41:39 -0400

    Seteando estilos basicos a widgets

--- src/components/1x2/personalRoll.tsx
+++ src/components/1x2/personalRoll.tsx
@@ -1,7 +1,11 @@
 export const PersonalRoll: React.FC = () => {
   return (
-    <div className='widget x1_y2'>
-      <h1>Aqui va mi FullStack</h1>
+    <div className='widget x1_y2'
+    style={{
+      backgroundColor: '#EBC351aa',
+    }}
+    >
+      <h1 style={{color: '#171717'}}>Aqui va mi FullStack</h1>
     </div>
   )
 }
